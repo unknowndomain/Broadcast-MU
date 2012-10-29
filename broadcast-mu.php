cf560292cc284e2bc2c3cd2ddf35b92338a3122c
Tom Lynch <me@unknowndomain.co.uk>
2012-10-29 01:45:44 +0000

    Version 1.0

---
+++ broadcast-mu.php
@@ -0,0 +1,136 @@
+<?
+
+/* 
+	Plugin Name: Broadcast MU
+	Description: Allows you to broadcast your post to multiple blogs on the same installation.
+	Version: 1.0 
+	Author: Tom Lynch 
+	Author URI: http://unknowndomain.co.uk/
+	Plugin URI: http://unknowndomain.co.uk/broadcast-mu
+
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+ *	BMU_compare
+ *	-----------
+ *	Used as part of the natural case-insensitive sort of the blog array in BMU_get_blogs_of_user().
+ */
+function BMU_compare($a, $b) {
+	return strnatcasecmp($a['blogname'], $b['blogname']);
+}
+
+/*
+ *	BMU_get_blogs_of_user
+ *	---------------------
+ *	Returns an array of blogs for the specified user (note it does not distinuguish between write and non-writeable blogs.
+ */
+function BMU_get_blogs_of_user($id) {
+	$users_blogs = get_blogs_of_user($id);
+	$i = 0;
+	foreach ($users_blogs as $blog) {
+		$output[$i]['userblog_id'] = $blog->userblog_id;
+		$output[$i]['blogname'] = $blog->blogname;
+		$output[$i]['domain'] = $blog->domain;
+		$output[$i]['path'] = $blog->path;
+		$output[$i]['site_id'] = $blog->site_id;
+		$output[$i]['siteurl'] = $blog->siteurl;
+		$i++;
+	}	
+	usort($output, "BMU_compare"); 
+	return $output;
+}
+
+/*
+ *	BMU_new_meta_boxes
+ *	------------------
+ *	Prints the meta box to the screen.
+ */
+function BMU_new_meta_boxes() {
+	global $post, $new_meta_boxes, $current_user;
+	
+	get_currentuserinfo();	
+	$users_blogs = BMU_get_blogs_of_user($current_user->ID);
+	
+	echo '<p>Post to:</p>';	
+	
+	foreach ($users_blogs as $blog):
+	
+		switch_to_blog($blog['userblog_id']);
+		if (current_user_can('publish_posts')):
+			restore_current_blog();
+			?>
+			<p><label <? if ($blog['siteurl'] != get_bloginfo('url')): ?>for="blog[<?= $blog['userblog_id'] ?>]"<? endif ?>><input type="checkbox" <? if ($blog['siteurl'] == get_bloginfo('url')): ?>checked="checked" disabled="disabled"<? else: ?>id="blog[<?= $blog['userblog_id'] ?>]" name="blog[<?= $blog['userblog_id'] ?>]"<? endif ?> /> <?= $blog['blogname'] ?></label></p>
+	<?
+		endif;
+	
+	endforeach;
+}
+
+/*
+ *	BMU_create_meta_box
+ *	-------------------
+ *	Creates the meta box and tells Wordpress that BMU_new_meta_boxes will print the contents.
+ */
+function BMU_create_meta_box() {
+	if ( function_exists('add_meta_box') ) {
+		add_meta_box('BMU_new-meta-boxes', 'Broadcast', 'BMU_new_meta_boxes', 'post', 'side', 'low' );
+	}
+}
+
+/*
+ *	BMU_save_postdata
+ *	-----------------
+ *	Runs after the post has been saved and sends post to other blogs.
+ */
+function BMU_save_postdata($post_id) {
+	if (did_action('save_post') <= count($_POST['blog'])-1) {
+		global $current_user;
+		get_currentuserinfo();	
+		$users_blogs = BMU_get_blogs_of_user($current_user->ID);
+		
+		$post = get_post($post_id, 'ARRAY_A');
+		$new_post['post_author'] = $post['post_author'];
+		$new_post['post_date'] = $post['post_date'];
+		$new_post['post_date_gmt'] = $post['post_date_gmt'];
+		$new_post['post_content'] = $post['post_content'];
+		$new_post['post_title'] = $post['post_title'];
+		$new_post['post_category'] = $post['post_category'];
+		$new_post['post_excerpt'] = $post['post_excerpt'];
+		$new_post['post_status'] = $post['post_status'];
+		$new_post['comment_status'] = $post['comment_status'];
+		$new_post['ping_status'] = $post['ping_status'];
+		$new_post['post_password'] = $post['post_password'];
+		$new_post['post_name'] = $post['post_name'];
+		$new_post['post_parent'] = $post['post_parent'];	
+		
+		foreach ($_POST['blog'] as $key => $value) {
+			$output[] = $key;		
+		}
+
+		foreach ($users_blogs as $blog) {
+			$in = array_search($blog['userblog_id'], $output);
+			if (is_int($in)) {
+				switch_to_blog($blog['userblog_id']);
+				if (current_user_can('publish_posts')) do_action('save_post', wp_insert_post($new_post));
+			}
+		}
+		restore_current_blog();
+	}
+}
+
+if ($_REQUEST['action'] != 'edit') add_action('admin_menu', 'BMU_create_meta_box');  
+if ($_REQUEST['action'] != 'edit') add_action('save_post', 'BMU_save_postdata');  
+
+?>
---
+++ readme.txt
@@ -0,0 +1,57 @@
+=== Broadcast MU ===
+Contributors: ctsttom
+Donate link: https://donate.cancerresearchuk.org/donate.asp
+Tags: post, broadcast, multicast, wpmu, mu, blogs
+Requires at least: 2.8
+Tested up to: 2.8
+Stable tag: 1.0
+
+Broadcast MU is a plugin for WordPress MU which allows you to broadcast a post to one or more other blogs on the same installation of WordPress MU.
+
+== Description ==
+
+Broadcast MU is a plugin for WordPress MU which allows you to broadcast a post to one or more other blogs on the same installation of WordPress MU by simply checking the box next to those you wish to send it to.
+
+You can enable this plugin on individual blogs or site wide.
+
+This plugin is for WordPress MU only because regular WordPress only supports having one blog anyway.
+
+== Installation ==
+
+1. Upload `broadcast-mu.php` to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the `Plugins` menu in WordPress MU.
+3. Create a new post from the `New Post` menu in WordPress MU and tick the blogs you want from the `Broadcast` box.
+
+== Frequently Asked Questions ==
+
+= Why can I only see some of the blogs on the site? =
+This is because Broadcast MU only allows you to post to blogs which you have access to publish on.
+
+= Can I enable the plugin for all users on my site? =
+Yes, Either upload it to the `/wp-content/mu-plugins/` directory or in the `Plugins` menu in WordPress MU click 'Activate site wide'.
+
+= Can I choose on which blogs my post will display? =
+Yes, simply click the blogs you want when posting in WordPress.
+
+= Can I modify or delete a post from all blogs simultaneously? =
+No, once broadcasted they cannot be modified or deleted simultaneously however you can still go and change them manually.
+
+== Screenshots ==
+
+1. New post screen with the Broadcast MU meta box.
+2. Close up of the Broadcast MU meta box.
+
+== Changelog ==
+
+= 1.0 =
+* Initial release.
+
+== Donate ==
+
+Please donate whatever you can afford to Cancer Research UK, their work has saved literally thousands of lives and together we can beat cancer.
+
+Thanks to Cancer Research UK and the NHS because without their help my families story could be a whole lot different right now.
+
+If you want to show thanks for the work I have done making this plugin then  please donate.
+
+Thanks.
